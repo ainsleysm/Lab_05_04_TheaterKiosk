e1d9cf88df82ee3be7273c8890526697b25224b0
ainsleysm <smithainsley21@gmail.com>
2022-09-22 20:05:18 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+       int age = 20;
+
+       if(age >= 21) {
+           System.out.println("You are 21 or over, here is your wristband!");
+       }
+    }
+}
